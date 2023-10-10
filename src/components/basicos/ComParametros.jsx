a0331ecef6d3295b03e0be298b82c48c2532abe2
José Ferreira <ferreirajosejp@gmail.com>
2023-10-10 17:37:54 -0300

    feat: add extensão jsx

---
+++ src/components/basicos/ComParametros.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ComParametro(props) {
+  return (
+    <div>
+      <p>{props.titulo}</p>
+      <p>{props.subTitulo}</p>
+    </div>
+  );
+}
---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Primeiro() {
+  const msg = 'seja bem vindo';
+
+  return `${msg} - Primeio Component`;
+}
